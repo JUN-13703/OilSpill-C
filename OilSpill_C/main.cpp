2008c78e1f6945f6779fd21ab9c15f6b240f9f5e
Jun Wei <junwei_chan@mymail.sutd.edu.sg>
2020-03-29 20:21:13 +0800

    Median filtering with demo images

--- OilSpill_C/main.cpp
+++ OilSpill_C/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char **argv)
 {
 	LibSeek::SeekThermal s_cam(argc == 2 ? argv[1] : "");
-	cv::Mat frame, g_frame;
+	cv::Mat frame, g_frame,  f_frame, b_frame;
     if (!s_cam.open()) {
         std::cout << "failed to open seek cam" << std::endl;
         return -1;
@@ -18,14 +18,18 @@ int main(int argc, char **argv)
         }
 
         cv::normalize(frame, g_frame, 0, 65535, cv::NORM_MINMAX);
-        cv::imshow("LWIR", g_frame);
+		//g_frame.convertTo(f_frame,  CV_32F);
+		cv::medianBlur(g_frame,b_frame,3);
+		//b_frame.convertTo(frame, CV_16U);
+		//cv::normalize(frame,  g_frame,0,65535,cv::NORM_MINMAX);
+        cv::imshow("LWIR", b_frame);
 
         //char c = cv::waitKey(10);
         //if (c == 's') {
 		//   cv::waitKey(0);
         //}
     //}
-	imwrite("./test.png",g_frame);
+	imwrite("./test_medfilt.png",b_frame);
 	
 	return 0;
 }

7a67385bb617c49c08b9b3be902f44bef2eb673a
Jun Wei <junwei_chan@mymail.sutd.edu.sg>
2020-07-01 10:09:56 +0800

    Continuous image capture.

--- OilSpill_C/main.cpp
+++ OilSpill_C/main.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <seek/seek.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <vector>
+#include <unistd.h>
+
+#define NUM_IMG 750
+#define DELAY_SEC 1
 
 int main(int argc, char **argv)
 {
@@ -11,25 +16,37 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    //while(1) {
+	int i = 0;
+	char img_name[96];
+	std::vector<int> cmp_params;
+	cmp_params.push_back(cv::IMWRITE_PNG_COMPRESSION);
+	cmp_params.push_back(0);
+	//double minT, maxT;
+    while(i < NUM_IMG) {
         if (!s_cam.read_raw_ffc_corrected(frame)) {
             std::cout << "no LWIR img" << std::endl;
             return -1;
         }
 
-        //cv::normalize(frame, g_frame, 0, 65535, cv::NORM_MINMAX);
-		//g_frame.convertTo(f_frame,  CV_32F);
-		cv::medianBlur(frame,b_frame,3);
-		//b_frame.convertTo(frame, CV_16U);
-		//cv::normalize(frame,  g_frame,0,65535,cv::NORM_MINMAX);
-        //cv::imshow("LWIR", b_frame);
-
-        //char c = cv::waitKey(10);
-        //if (c == 's') {
-		//   cv::waitKey(0);
-        //}
-    //}
-	imwrite("./temptest.png",b_frame);
+		cv::medianBlur(frame,g_frame,3);
+		cv::normalize(g_frame, b_frame, 0, 65535, cv::NORM_MINMAX);
+		g_frame.convertTo(f_frame,  CV_32F);
+		//f_frame = 0.5*0.03762687965*f_frame-273.15; //0.03762687965
+		sprintf(img_name,"/home/pi/Desktop/OilSpill-C/OilSpill_C/Debug/thermv_images/i%d.png",i);
+		imwrite(img_name,b_frame);
+		sprintf(img_name,"/home/pi/Desktop/OilSpill-C/OilSpill_C/Debug/raw_images/r%d.png",i);
+		imwrite(img_name,frame);
+		//sprintf(img_name,"radiometry/t%d.tiff",i);
+		//cv::minMaxLoc(f_frame,&minT,&maxT);
+		//std::cout<<"Max T: "<<maxT<<" Min T: "<<minT<<std::endl;
+		//maxT = 0;
+		//minT = 100;
+		//imwrite(img_name,f_frame);
+		i++;
+		sleep(DELAY_SEC);
+	}
+	sleep(2);
+	system("shutdown");
 	
 	return 0;
 }
